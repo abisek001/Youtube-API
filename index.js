067fe474b89cf7fe4913d7678d3195e9b7ac4360
abisek001 <abisek501@gmail.com>
2023-12-13 21:03:05 +0530

    try to connect with server and client

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
-require('dotenv').config();
+dotenv.config();
 const port = process.env.PORT;
 const hostname = process.env.HOST;
 
 
 const Atlas_Url = process.env.DBURL;
 
-mongoose.connect(Atlas_Url,{useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(Atlas_Url)
     .then(response => {
         app.listen(port, hostname, () => {
             console.log(`server is runing on http://${hostname}:${port}`);
