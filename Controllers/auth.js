d72599aecea4e0f0e524ee8e2bc63d452804aae5
abisek001 <abisek501@gmail.com>
2024-01-15 15:04:24 +0530

    google login created

--- Controllers/auth.js
+++ Controllers/auth.js
@@ -1,14 +1,35 @@
 import jwt from 'jsonwebtoken';
 import Users from '../Models/User.js';
 
-const login = async(req, res) => {
-    const {email} = req.body;
-    console.log(req.body);  
+const login = async (req, res) => {
+    const { email } = req.body;
+    console.log(email);
     try {
-        const existingUser = await Users.findOne({email});
+        const existingUser = await Users.findOne({ email });
+        if (!existingUser) {
+            try {
+                const newUser = await Users.create({ email })
+                const token = jwt.sign({
+                    email: newUser.email, id: newUser._id
+                }, process.env.JWT_SECRET, {
+                    expiresIn: "1h"
+                })
+                res.status(200).json({ result: newUser, token })
+            } catch (error) {
+                res.status(500).json({ message: "somthing wents wrong..." })
+            }
+        }
+        else {
+            const token = jwt.sign({
+                email: existingUser.email, id: existingUser._id
+            }, process.env.JWT_SECRET, {
+                expiresIn: "1h"
+            })
+            res.status(200).json({ result: existingUser, token })
+        }
     }
-    catch{
-
+    catch {
+        res.status(500).json({message: "somting wents wrong ..."})
     }
 }
 
